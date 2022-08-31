c4262f0bcfe91c37e8fab714ed1e86ff23b9daf4
Karla Elliott <karla.elliott@taillight.com>
2022-08-31 11:01:51 -0500

    Adding files

---
+++ static/js/app.js
@@ -0,0 +1,27 @@
+function pricePrediction(){
+    var t = d3.select('results')
+
+    var origin = document.getElementById("origin");
+    var valueOrigin = origin.value;
+    var destination = document.getElementById("destination");
+    var valueDestination = destination.value;
+    var weather = document.getElementById("weather");
+    var valueWeather = weather.value;
+    var vehicleType = document.getElementById("vehicleType");
+    var valueVehType = vehicleType.value;
+    var weekDay = document.getElementById("weekDay");
+    var valueWeekDay = weekDay.value;
+
+    
+    d3.json(`https://ulprediction.herokuapp.com/api/prediction/${valueOrigin}/${valueDestination}/${valueWeather}/${valueVehType}/${valueWeekDay}`, {
+    //d3.json(`http://localhost:5000/api/prediction/${valueOrigin}/${valueDestination}/${valueWeather}/${valueVehType}/${valueWeekDay}`, {
+      method:"GET"
+    })
+    .then(json => {
+        document.getElementById('bestResults').innerText = 'Your best choice would be: ';
+        document.getElementById('bestCarType').innerText = json.type;
+        document.getElementById('bestPrice').innerText = json.price;
+    });
+
+}
+
